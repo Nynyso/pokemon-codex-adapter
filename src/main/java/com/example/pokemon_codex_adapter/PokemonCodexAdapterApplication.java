96f736dd7af9dbbb5432e00df589052fe5583614
Gianni Carignano <gianni.carignano@gmail.com>
2026-06-12 21:35:17 +0200

    Added initial app version

---
+++ src/main/java/com/example/pokemon_codex_adapter/PokemonCodexAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokemon_codex_adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonCodexAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonCodexAdapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pokemon_codex_adapter/PokemonCodexAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemon_codex_adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonCodexAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
